8988da842a79faeb3b6c75f73ea3180c755e4ed8
gaferia <165069257+gaferiaalte@users.noreply.github.com>
2024-06-01 06:19:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-b363b01f-ee9d-4b3d-a7bc-8db887b0d565/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-b29d5a61-0682-46f8-8cc3-c9146e485ec9/)
 
 
 echo "$curl_output" >> Update.tsx
